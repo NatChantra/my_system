30be66e4ddd62cdb2cd7b04e3d4ac389d2139307
NatChantra <natchantra2005@email@gmail.com>
2026-07-13 16:10:01 +0700

    Fix Dockerfile and CORS

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,12 @@
 <?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // សម្រាប់សាកល្បង
+    'allowed_origins' => ['https://venol-frontend.onrender.com'], // ដាក់ URL នៃ Frontend របស់បងនៅទីនេះ
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => true,
 ];
