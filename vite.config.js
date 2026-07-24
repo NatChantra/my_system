fe73a49f313f12440be229ef6a7cfc441f1920e8
NatChantra <natchantra2005@email@gmail.com>
2026-07-24 10:00:24 +0700

    fix primary key and update ip

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+            proxy: {
+          '/api': {
+            target: 'http://192.168.100.67:8000',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
       }
     }
-  }
-})
+)
